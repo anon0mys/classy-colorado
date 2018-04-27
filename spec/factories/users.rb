18e44c91cbffa8360dba36bdc0db44c08ac5bcff
Evan Wheeler <anon0mys87@gmail.com>
2018-04-26 23:22:31 -0600

    Adds role enum to users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :user do
-    email "test@mail.com"
+    sequence(:email) { "test#{Random.rand(10000).to_s}@mail.com" }
     password "password"
+  end
+
+  factory :admin, class: User do
+    sequence(:email) { "admin#{Random.rand(10000).to_s}@mail.com" }
+    password 'password'
+    role 1
   end
 end
